1cb4bcf50342074c6bacb9fc49b9925fe8d3cbda
ursusma <736324424@qq.com>
2016-10-13 16:03:23 +0800

    remove login

--- huiu/huiuapp/urls.py
+++ huiu/huiuapp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',auth_views.login,{'template_name':'huiu/login.html'}),
+    url(r'^$',views.login),
     url(r'^index/$',views.index),
 ]
